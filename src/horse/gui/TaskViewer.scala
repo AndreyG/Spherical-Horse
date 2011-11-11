b88c46879ca4aa5ab4b3e6af767f1a0e53748b2c
Andrey Davydov <andrey.a.davydov@gmail.com>
2011-11-11 23:15:03 +0400

    Almost working problem checker.

--- src/horse/gui/TaskViewer.scala
+++ src/horse/gui/TaskViewer.scala
@@ -1,8 +1,33 @@
 package horse.gui
 
+import scala.swing.{BorderPanel, Frame, MenuBar, MenuItem, Action}
+import javax.swing.KeyStroke
+import BorderPanel.Position._
+import java.awt.Dimension
+import java.awt.event.KeyEvent._
+
 import horse.core.fieldstate.StaticField
+
+import horse.Config
 
 object TaskViewer {
     def apply(etalon: StaticField) {
+        val frame = new Frame {
+            contents = new BorderPanel {
+                add(new FieldImage(etalon), Center)
+                menuBar = new MenuBar {
+                    contents += new MenuItem(new Action("Check") {
+                        accelerator = Some(KeyStroke.getKeyStroke(VK_C, 0, true))
+                        override def apply() {
+                            println("Check")
+                        }
+                    })
+                }
+            }
+
+            size = new Dimension(Config.getInt("field-image.width"), Config.getInt("field-image.height"))
+            resizable = false
+        }
+        frame.visible = true
     }
 }
